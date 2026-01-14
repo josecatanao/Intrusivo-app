39c595efc15b40e18aea565c5f6be85b7b5bb6fa
josé neto <josecatanaoneto@gmail.com>
2026-01-14 09:38:12 -0300

    feat: initial project setup

---
+++ app/auth/cadastro-conta.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function CadastroConta(){
+    return <Text>CadastroConta</Text>
+} 
---
+++ app/auth/login-email-senha.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function LoginEmailSenha(){
+    return <Text>LoginEmailSenha</Text>
+} 
